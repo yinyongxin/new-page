efe65e6eb18945c799b79c12c8598e2b8e417034
yinyongxin0220 <yinyongxin0220@gmail.com>
2024-12-12 06:42:24 +0000

    feat: update

--- src/layout/nev-bar/index.tsx
+++ src/layout/nev-bar/index.tsx
@@ -1,5 +1,17 @@
+import { cn } from "../../utils/style";
+import classes from "./index.module.css";
+const list = [
+  {
+    icon: "",
+  },
+];
 const NavBar = () => {
-  return <nav>NavBar</nav>;
+  return (
+    <nav class={cn(classes.navBar, "relative px-4 py-2")}>
+      <div class="absolute inset-0 bg-black rounded-2xl overflow-hidden"></div>
+      <div class="relative text-white">NavBar</div>
+    </nav>
+  );
 };
 
 export default NavBar;
