b3d46f9e1758b9a5df0125b57a91b71787273260
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2024-12-15 01:03:04 +0800

    build: 添加 GridStack 额外样式
    - 在项目中引入 gridstack-extra.min.css 文件
    - 此修改将增强 GridStack 的视觉效果和功能性

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { render } from "solid-js/web";
 import "./styles/tailwind/index.css";
 import "./styles/index.css";
 import "gridstack/dist/gridstack.min.css";
+import "gridstack/dist/gridstack-extra.min.css";
 
 import App from "./App";
 
