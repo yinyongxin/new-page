f4a09bea7318983420cc169f9ea1c38853fff17d
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2024-12-18 01:08:53 +0800

    refactor(components): 重构导航栏组件布局和样式
    - 更新了设置组件的布局，将选项排列成网格
    - 为导航栏组件添加了阴影样式
    - 优化了代码格式和结构，提高了可读性

--- src/components/nav-bar/comps/setting/index.tsx
+++ src/components/nav-bar/comps/setting/index.tsx
@@ -4,75 +4,79 @@ import { AppContext } from "../../../../app-conetent";
 import Drawer from "../../../drawer";
 
 const Setting = () => {
-  let settingRef!: HTMLDivElement;
+	let settingRef!: HTMLDivElement;
 
-  const { navBar, setNavBar } = useContext(AppContext);
-  const [open, setOpen] = createSignal(true);
+	const { navBar, setNavBar } = useContext(AppContext);
+	const [open, setOpen] = createSignal(true);
 
-  const drawerPositionObj = {
-    left: "pageRight",
-    right: "pageLeft",
-    top: "pageBottom",
-    bottom: "pageTop",
-  } as const;
+	const drawerPositionObj = {
+		left: "pageRight",
+		right: "pageLeft",
+		top: "pageBottom",
+		bottom: "pageTop",
+	} as const;
 
-  return (
-    <>
-      <div
-        ref={settingRef}
-        class="h-12 w-12 bg-black/20 rounded-xl cursor-pointer flex justify-center items-center"
-        onclick={() => {
-          setOpen(!open());
-        }}
-      >
-        <Settings class="animate-[spin_3s_linear_infinite]" />
-      </div>
-      <Drawer
-        open={open()}
-        position={drawerPositionObj[navBar.position]}
-        alignment="1rem"
-      >
-        <div class="p-4">
-          <div
-            onClick={() => {
-              setNavBar?.({
-                position: "top",
-              });
-            }}
-          >
-            上
-          </div>
-          <div
-            onClick={() => {
-              setNavBar?.({
-                position: "right",
-              });
-            }}
-          >
-            右
-          </div>
-          <div
-            onClick={() => {
-              setNavBar?.({
-                position: "bottom",
-              });
-            }}
-          >
-            下
-          </div>
-          <div
-            onClick={() => {
-              setNavBar?.({
-                position: "left",
-              });
-            }}
-          >
-            左
-          </div>
-        </div>
-      </Drawer>
-    </>
-  );
+	return (
+		<>
+			<div
+				ref={settingRef}
+				class="h-12 w-12 bg-black/20 rounded-xl cursor-pointer flex justify-center items-center"
+				onclick={() => {
+					setOpen(!open());
+				}}
+			>
+				<Settings class="animate-[spin_3s_linear_infinite]" />
+			</div>
+			<Drawer
+				open={open()}
+				position={drawerPositionObj[navBar.position]}
+				alignment="1rem"
+			>
+				<div class="p-4 flex gap-4">
+					<div
+						class="h-12 w-12 bg-black/20 rounded-xl cursor-pointer flex justify-center items-center"
+						onClick={() => {
+							setNavBar?.({
+								position: "top",
+							});
+						}}
+					>
+						上
+					</div>
+					<div
+						class="h-12 w-12 bg-black/20 rounded-xl cursor-pointer flex justify-center items-center"
+						onClick={() => {
+							setNavBar?.({
+								position: "right",
+							});
+						}}
+					>
+						右
+					</div>
+					<div
+						class="h-12 w-12 bg-black/20 rounded-xl cursor-pointer flex justify-center items-center"
+						onClick={() => {
+							setNavBar?.({
+								position: "bottom",
+							});
+						}}
+					>
+						下
+					</div>
+					<div
+						class="h-12 w-12 bg-black/20 rounded-xl cursor-pointer flex justify-center items-center"
+						onClick={() => {
+							setNavBar?.({
+								position: "left",
+							});
+						}}
+					>
+						左
+					</div>
+				</div>
+			</Drawer>
+		</>
+	);
 };
 
 export default Setting;
--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -3,49 +3,45 @@ import { cn } from "../../utils/style";
 import classes from "./index.module.css";
 import { ParentComponent, useContext } from "solid-js";
 import { AppContext } from "../../app-conetent";
-import Setting from "./comps/setting";
-import AddWebsite from "./comps/add-website";
-import Item from "./item";
 
 const positionOptions: Record<Position.types, string> = {
-  top: "top-4 left-1/2 -translate-x-1/2",
-  right: "right-4 top-1/2 -translate-y-1/2",
-  bottom: "bottom-4 left-1/2 -translate-x-1/2",
-  left: "left-4 top-1/2 -translate-y-1/2",
+	top: "top-4 left-1/2 -translate-x-1/2",
+	right: "right-4 top-1/2 -translate-y-1/2",
+	bottom: "bottom-4 left-1/2 -translate-x-1/2",
+	left: "left-4 top-1/2 -translate-y-1/2",
 };
 
-const navBarVariants = cva("absolute p-3 rounded-xl z-100s", {
-  variants: {
-    position: positionOptions,
-  },
-  defaultVariants: {
-    position: "left",
-  },
+const navBarVariants = cva("absolute p-3 rounded-xl z-100s shadow", {
+	variants: {
+		position: positionOptions,
+	},
+	defaultVariants: {
+		position: "left",
+	},
 });
 
-const NavBar:ParentComponent = (props) => {
-  const { navBar, appContext } = useContext(AppContext);
+const NavBar: ParentComponent = (props) => {
+	const { navBar, appContext } = useContext(AppContext);
 
-  return (
-    <nav
-      class={cn(navBarVariants({ position: navBar.position }), classes.navBar)}
-    >
-      <div
-        class={cn("absolute inset-0 rounded-2xl overflow-hidden", {
-          "backdrop-blur": appContext.blur,
-          "bg-white/30": appContext.blur,
-        })}
-      ></div>
-      <div
-        class={cn("relative flex gap-2 text-white", {
-          "flex-col": navBar.position === "left" || navBar.position === "right",
-        })}
-      >
-        {props.children}
-       
-      </div>
-    </nav>
-  );
+	return (
+		<nav
+			class={cn(navBarVariants({ position: navBar.position }), classes.navBar)}
+		>
+			<div
+				class={cn("absolute inset-0 rounded-2xl overflow-hidden", {
+					"backdrop-blur": appContext.blur,
+					"bg-white/30": appContext.blur,
+				})}
+			></div>
+			<div
+				class={cn("relative flex gap-2 text-white", {
+					"flex-col": navBar.position === "left" || navBar.position === "right",
+				})}
+			>
+				{props.children}
+			</div>
+		</nav>
+	);
 };
 
 export default NavBar;
