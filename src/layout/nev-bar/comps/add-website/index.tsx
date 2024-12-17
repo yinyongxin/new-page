42dba20284f694dd009b7067255e2d9c4faacadb
yinyongxin0220 <yinyongxin0220@gmail.com>
2024-12-17 03:26:43 +0000

    feat：update

--- src/layout/nev-bar/comps/add-website/index.tsx
+++ src/layout/nev-bar/comps/add-website/index.tsx
@@ -18,12 +18,16 @@ const AddWebsite = () => {
         center
         triggerElement={ref}
         active="click"
+        defaultOpen
       >
-        <div class="grid grid-cols-4 gap-4 p-4">
-          <div class="aspect-square">1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
+        <div class="p-4">
+          <div class="relative rounded-lg w-full overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-0 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:-z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-12 after:top-3 after:rounded-full after:blur-lg">
+            <input
+              placeholder="网站地址"
+              class="relative bg-transparent ring-0 outline-none border border-neutral-200 text-neutral-900 placeholder-violet-700 text-sm rounded-lg focus:ring-violet-500 placeholder-opacity-60 focus:border-violet-500 block w-full p-2.5 checked:bg-emerald-500"
+              type="text"
+            />
+          </div>
         </div>
       </AppPupop>
     </>
