db0aa0cc3fbf70fec91c1874f4a867bb73a61400
yinyongxin0220 <yinyongxin0220@gmail.com>
2024-12-26 02:46:37 +0000

    feat: update

--- src/components/nav-bar/comps/add-website/index.tsx
+++ src/components/nav-bar/comps/add-website/index.tsx
@@ -1,35 +1,34 @@
-import AppBox from "../../../app-box";
 import AppPupop from "../../../app-pupop";
 import CirclePlus from "lucide-solid/icons/circle-plus";
 import Item from "../../item";
 
 const AddWebsite = () => {
-	let ref!: HTMLDivElement;
+  let ref!: HTMLDivElement;
 
-	return (
-		<>
-			<Item ref={ref}>
-				<CirclePlus />
-			</Item>
-			<AppPupop
-				width="30rem"
-				height="20rem"
-				center
-				triggerElement={ref}
-				active="click"
-			>
-				<div class="p-4">
-					<div class="relative rounded-lg w-full overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-0 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:-z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-12 after:top-3 after:rounded-full after:blur-lg">
-						<input
-							placeholder="网站地址"
-							class="relative bg-transparent ring-0 outline-none border border-neutral-200 text-neutral-900 placeholder-violet-700 text-sm rounded-lg focus:ring-violet-500 placeholder-opacity-60 focus:border-violet-500 block w-full p-2.5 checked:bg-emerald-500"
-							type="text"
-						/>
-					</div>
-				</div>
-			</AppPupop>
-		</>
-	);
+  return (
+    <>
+      <Item ref={ref}>
+        <CirclePlus />
+      </Item>
+      <AppPupop
+        width="30rem"
+        height="20rem"
+        center
+        triggerElement={ref}
+        active="click"
+      >
+        <div class="p-4">
+          <div class="relative rounded-lg w-full overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-0 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:-z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-12 after:top-3 after:rounded-full after:blur-lg">
+            <input
+              placeholder="网站地址"
+              class="relative bg-transparent ring-0 outline-none border border-neutral-200 text-neutral-900 placeholder-violet-700 text-sm rounded-lg focus:ring-violet-500 placeholder-opacity-60 focus:border-violet-500 block w-full p-2.5 checked:bg-emerald-500"
+              type="text"
+            />
+          </div>
+        </div>
+      </AppPupop>
+    </>
+  );
 };
 
 export default AddWebsite;
--- src/components/nav-bar/item.tsx
+++ src/components/nav-bar/item.tsx
@@ -1,27 +1,27 @@
-import { JSX, ParentComponent, ParentProps, Show } from "solid-js";
+import { JSX, ParentComponent, ParentProps } from "solid-js";
 import AppBox from "../app-box";
 
 type ItemProps = {
-	ref?: HTMLDivElement;
-	open?: boolean;
+  ref?: HTMLDivElement;
+  open?: boolean;
 } & ParentProps<
-	Pick<JSX.CustomEventHandlersCamelCase<HTMLDivElement>, "onClick">
+  Pick<JSX.CustomEventHandlersCamelCase<HTMLDivElement>, "onClick">
 >;
 const Item: ParentComponent<ItemProps> = (props) => {
-	return (
-		<AppBox
-			ref={props.ref}
-			type={props.open ? "light" : "dark"}
-			class="h-12 w-12 cursor-pointer flex justify-center items-center relative"
-			onClick={props.onClick}
-			shadow
-		>
-			{props.children}
-			{/* <Show when={props.open}>
+  return (
+    <AppBox
+      ref={props.ref}
+      type={props.open ? "light" : "dark"}
+      class="h-12 w-12 cursor-pointer flex justify-center items-center relative"
+      onClick={props.onClick}
+      shadow
+    >
+      {props.children}
+      {/* <Show when={props.open}>
 				<div class="absolute -bottom-2 w-1 h-1 bg-gray-500 rounded-full"></div>
 			</Show> */}
-		</AppBox>
-	);
+    </AppBox>
+  );
 };
 
 export default Item;
