a3e4d4ab1e53f920732390810e2062572a289d48
yinyongxin0220 <yinyongxin0220@gmail.com>
2024-12-27 09:36:15 +0000

    feat: update

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -3,35 +3,36 @@ import { AppContext } from "../app-conetent";
 import { cn } from "../utils/style";
 
 const Background = () => {
-	const { background } = useContext(AppContext);
-	const getStyle = () => {
-		if (background.type === "image") {
-			return {
-				"background-image": `url(${background.image.src})`,
-			};
-		}
-	};
-	return (
-		<div
-			class={cn("absolute inset-0 bg-cover bg-center", {
-				[background.className]: background.type === "css",
-			})}
-			style={getStyle()}
-		>
-			<div
-				class={cn(
-					"h-full w-full bg-black/30",
-					background.backdropBlur.flag
-						? {
-								"backdrop-blur-sm": background.backdropBlur.size === "sm",
-								"backdrop-blur": background.backdropBlur.size === "default",
-								"backdrop-blur-md": background.backdropBlur.size === "md",
-						  }
-						: {}
-				)}
-			/>
-		</div>
-	);
+  const { background } = useContext(AppContext);
+  const getStyle = () => {
+    if (background.type === "image") {
+      return {
+        "background-image": `url(${background.image.src})`,
+      };
+    }
+  };
+  return (
+    <div
+      class={cn("absolute inset-0 bg-cover bg-center", {
+        [background.className]: background.type === "css",
+      })}
+      style={getStyle()}
+    >
+      <div
+        class={cn("h-full w-full", {
+          "backdrop-blur-sm bg-white/30":
+            background.backdropBlur.flag &&
+            background.backdropBlur.size === "sm",
+          "backdrop-blur bg-white/30":
+            background.backdropBlur.flag &&
+            background.backdropBlur.size === "default",
+          "backdrop-blur-md bg-white/30":
+            background.backdropBlur.flag &&
+            background.backdropBlur.size === "md",
+        })}
+      />
+    </div>
+  );
 };
 
 export default Background;
--- src/full-windows/system-settings/index.tsx
+++ src/full-windows/system-settings/index.tsx
@@ -7,71 +7,71 @@ import { cn } from "../../utils/style";
 import BgSetting from "./bg-setting";
 
 const SystemSettings = () => {
-	const { navBar, setNavBar, fullWindows } = useContext(AppContext);
+  const { navBar, setNavBar, fullWindows } = useContext(AppContext);
 
-	return (
-		<FullScreen open={fullWindows.current === "system-settings"}>
-			<div class="h-full overflow-auto">
-				<div
-					class={cn(
-						"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 grid-flow-dense"
-					)}
-					// style={{ "grid-auto-rows": " minmax(100px, auto)" }}
-				>
-					<div>
-						<AppText title={6}>导航栏设置</AppText>
-						<div class={cn("grid grid-cols-4 gap-4 mt-2")}>
-							<AppBox
-								onClick={() => {
-									setNavBar?.({ position: "top" });
-								}}
-								class="aspect-square flex flex-col items-center gap-2 p-2"
-								blur={{ flag: false }}
-								bgFreground={navBar.position === "top"}
-							>
-								<AppBox class="h-1/4 w-2/3" blur={{ flag: false }}></AppBox>
-								<AppBox class="flex-1 w-full" blur={{ flag: false }}></AppBox>
-							</AppBox>
+  return (
+    <FullScreen open={fullWindows.current === "system-settings"}>
+      <div class="h-full overflow-auto">
+        <div
+          class={cn(
+            "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 grid-flow-dense"
+          )}
+          // style={{ "grid-auto-rows": " minmax(100px, auto)" }}
+        >
+          <div>
+            <AppText title={6}>导航栏设置</AppText>
+            <div class={cn("grid grid-cols-4 gap-4 mt-2")}>
+              <AppBox
+                onClick={() => {
+                  setNavBar?.({ position: "top" });
+                }}
+                class="aspect-square flex flex-col items-center gap-2 p-2"
+                blur={{ flag: false }}
+                bgFreground={navBar.position === "top"}
+              >
+                <AppBox class="h-1/4 w-2/3" blur={{ flag: false }}></AppBox>
+                <AppBox class="flex-1 w-full" blur={{ flag: false }}></AppBox>
+              </AppBox>
 
-							<AppBox
-								onClick={() => {
-									setNavBar?.({ position: "right" });
-								}}
-								bgFreground={navBar.position === "right"}
-								class="aspect-square flex flex-row-reverse items-center gap-2 p-2"
-								blur={{ flag: false }}
-							>
-								<AppBox class="w-1/4 h-2/3" blur={{ flag: false }}></AppBox>
-								<AppBox class="flex-1 h-full" blur={{ flag: false }}></AppBox>
-							</AppBox>
-							<AppBox
-								onClick={() => {
-									setNavBar?.({ position: "bottom" });
-								}}
-								bgFreground={navBar.position === "bottom"}
-								class="aspect-square flex flex-col-reverse items-center gap-2 p-2"
-								blur={{ flag: false }}
-							>
-								<AppBox class="h-1/4 w-2/3" blur={{ flag: false }}></AppBox>
-								<AppBox class="flex-1 w-full" blur={{ flag: false }}></AppBox>
-							</AppBox>
-							<AppBox
-								onClick={() => {
-									setNavBar?.({ position: "left" });
-								}}
-								bgFreground={navBar.position === "left"}
-								class="aspect-square flex items-center gap-2 p-2"
-								blur={{ flag: false }}
-							>
-								<AppBox class="w-1/4 h-2/3" blur={{ flag: false }}></AppBox>
-								<AppBox class="flex-1 h-full" blur={{ flag: false }}></AppBox>
-							</AppBox>
-						</div>
-					</div>
-					<BgSetting />
-				</div>
-			</div>
-		</FullScreen>
-	);
+              <AppBox
+                onClick={() => {
+                  setNavBar?.({ position: "right" });
+                }}
+                bgFreground={navBar.position === "right"}
+                class="aspect-square flex flex-row-reverse items-center gap-2 p-2"
+                blur={{ flag: false }}
+              >
+                <AppBox class="w-1/4 h-2/3" blur={{ flag: false }}></AppBox>
+                <AppBox class="flex-1 h-full" blur={{ flag: false }}></AppBox>
+              </AppBox>
+              <AppBox
+                onClick={() => {
+                  setNavBar?.({ position: "bottom" });
+                }}
+                bgFreground={navBar.position === "bottom"}
+                class="aspect-square flex flex-col-reverse items-center gap-2 p-2"
+                blur={{ flag: false }}
+              >
+                <AppBox class="h-1/4 w-2/3" blur={{ flag: false }}></AppBox>
+                <AppBox class="flex-1 w-full" blur={{ flag: false }}></AppBox>
+              </AppBox>
+              <AppBox
+                onClick={() => {
+                  setNavBar?.({ position: "left" });
+                }}
+                bgFreground={navBar.position === "left"}
+                class="aspect-square flex items-center gap-2 p-2"
+                blur={{ flag: false }}
+              >
+                <AppBox class="w-1/4 h-2/3" blur={{ flag: false }}></AppBox>
+                <AppBox class="flex-1 h-full" blur={{ flag: false }}></AppBox>
+              </AppBox>
+            </div>
+          </div>
+          <BgSetting />
+        </div>
+      </div>
+    </FullScreen>
+  );
 };
 export default SystemSettings;
