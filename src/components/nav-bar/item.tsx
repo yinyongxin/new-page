7f7c1a26a2879f312972fbb7c9dadca7ae7be92e
CurryYYX <52994935+yinyongxin@users.noreply.github.com>
2024-12-27 21:48:25 +0800

    refactor(components): 移除导航栏中的添加网站功能
    - 删除了 AddWebsite 组件
    - 优化了 Item 组件的代码格式

--- src/components/nav-bar/item.tsx
+++ src/components/nav-bar/item.tsx
@@ -4,33 +4,33 @@ import { cn } from "../../utils/style";
 import { AppContext } from "../../app-conetent";
 
 type ItemProps = {
-  ref?: HTMLDivElement;
-  open?: boolean;
+	ref?: HTMLDivElement;
+	open?: boolean;
 } & ParentProps<
-  Pick<JSX.CustomEventHandlersCamelCase<HTMLDivElement>, "onClick">
+	Pick<JSX.CustomEventHandlersCamelCase<HTMLDivElement>, "onClick">
 >;
 const Item: ParentComponent<ItemProps> = (props) => {
-  const { navBar } = useContext(AppContext);
-  return (
-    <AppBox
-      ref={props.ref}
-      class="h-12 w-12 cursor-pointer flex justify-center items-center relative"
-      onClick={props.onClick}
-      shadow
-    >
-      {props.children}
-      <Show when={props.open}>
-        <div
-          class={cn("absolute w-1 h-1 bg-gray-500 rounded-full", {
-            "-top-2": navBar.position === "top",
-            "-right-2": navBar.position === "right",
-            "-bottom-2": navBar.position === "bottom",
-            "-left-2": navBar.position === "left",
-          })}
-        ></div>
-      </Show>
-    </AppBox>
-  );
+	const { navBar } = useContext(AppContext);
+	return (
+		<AppBox
+			ref={props.ref}
+			class="h-12 w-12 cursor-pointer flex justify-center items-center relative"
+			onClick={props.onClick}
+			shadow
+		>
+			{props.children}
+			<Show when={props.open}>
+				<div
+					class={cn("absolute w-1 h-1 bg-gray-500 rounded-full", {
+						"-top-2": navBar.position === "top",
+						"-right-2": navBar.position === "right",
+						"-bottom-2": navBar.position === "bottom",
+						"-left-2": navBar.position === "left",
+					})}
+				></div>
+			</Show>
+		</AppBox>
+	);
 };
 
 export default Item;
